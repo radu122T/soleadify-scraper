6f6c2042b71049386b0225cfe428b4d9c6a74cd9
Radu Cudalb <radu.cudalb@yahoo.ro>
2023-07-25 12:29:22 +0300

    scraper

--- dist/controller/scraper.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { KafkaContext } from "@nestjs/microservices";
-import { ScraperService } from "src/services/scraper.service";
-export declare class ScraperController {
-    private scraperService;
-    constructor(scraperService: ScraperService);
-    handleWebsiteScraping(message: any, context: KafkaContext): Promise<void>;
-}
--- dist/dto/create.user.dto.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class Website {
-    domain: string;
-    company_commercial_name?: string;
-    company_legal_name?: string;
-    company_all_available_names?: string;
-    phone_numbers?: string[];
-    social_media?: string[];
-    address?: string[];
-}
